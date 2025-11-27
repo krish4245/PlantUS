a2f71bcb06f5c6b35b52556926f39b4170aa5a1d
SDC-USER <sdc-user@VY-7F-IOSTC-036.local>
2025-11-27 16:48:02 +0530

    search screen commited

---
+++ garden_app/View/SearchCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  SearchCollectionViewCell.swift
+//  garden_app
+//
+//  Created by SDC-USER on 27/11/25.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var taglineLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var plantImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ garden_app/View/Search_2CollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  Search_2CollectionViewCell.swift
+//  garden_app
+//
+//  Created by SDC-USER on 27/11/25.
+//
+
+import UIKit
+
+class Search_2CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var maintenanceLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var plantImageView: UIImageView!
+    @IBOutlet weak var lightLabel: UILabel!       // For "Bright Indirect"
+    @IBOutlet weak var waterLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
