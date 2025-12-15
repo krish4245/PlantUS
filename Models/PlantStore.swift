e0f5c168ee2ff03967e98d8b963d942d72d7e70e
krish4245 <krishna.27.5.2005@gmail.com>
2025-12-15 11:00:11 +0530

    Add .gitignore

---
+++ Models/PlantStore.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class PlantStore {
+    static let shared = PlantStore()
+
+    private init() {}
+
+    // User's real plants (initially empty)
+    var plants: [PlantModel] = []
+
+    // Spaces (initially empty)
+    var spaces: Int {
+        return 0 // until you add a real feature
+    }
+
+    var totalPlants: Int {
+        return plants.count
+    }
+}
